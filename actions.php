b8dc8b057f69bf106bef471ccd7c4599288bec29
centipedes <kylaslea@gmail.com>
2016-11-29 17:49:26 -0500

    Update actions.php

--- actions.php
+++ actions.php
@@ -4,6 +4,7 @@ session_start();
 
 include('connDb.php');
 
+
 // Connect to DB
 
 $actionSelect = $_POST['actionSelect'];
@@ -16,9 +17,9 @@ switch($actionSelect){
  		$password    =    $_POST['pw-reg'];
 		$password    =    md5($password);
 		$email       =    $_POST['email-reg'];
-		$query    =    "insert into users(ID,username,password,email, status)values('','$userName','$password','$email', 1)";
+		$query    =    "insert into users(username,password,email, status)values('$userName','$password','$email', 1)";
 		$res    =    mysql_query($query);		
-		echo 'Thanks, '.$userName.', you are now registered for Virbizz!';		
+		echo 'Thanks, '.$userName.', you are now registered for Velocity!';		
 		header("Location: index.php");		
 		die();
 			
@@ -30,19 +31,51 @@ switch($actionSelect){
 		$password    =    $_POST['pw-login'];
 		$password    =    md5($password);
 		
-		$query = "select * from users where username='$userName' and password='$password'"; 
-		$res    =    mysql_query($query);
-		$rows = mysql_num_rows($res); 
+		$query 	= "select * from users where username='$userName' and password='$password'"; 
+		$res    = mysql_query($query);
+		$row	= mysql_fetch_array($res);
+		$rows 	= mysql_num_rows($res); 
 		//echo $rows;
 		if($rows==1) {
 			$_SESSION["userName"] = $userName ;
 			$_SESSION["password"] = $password ;
+			$_SESSION["id"]       = $row["ID"];
+		} else {
+			header("Location: login.php");		
+			die();
 		}
 		header("Location: index.php");		
 		die();
 
 	break;
-
+	
+	case 'offer-ride':
+		
+		$startPostal 	= $_POST['startPostal'];
+		$endPostal		= $_POST['endPostal'];
+		$offererId		= $_SESSION['id'];
+		
+		$query    =    "insert into rides(offerID,start_postal, end_postal, status)values('$offererId','$startPostal','$endPostal', 1)";
+		$res    =    mysql_query($query);		
+		
+		echo 'Thanks, '.$_SESSION['userName'].', your ride has been created. <br />';
+		echo 'Click <a href="./">here</a> to go home.';	
+	
+	break;
+	
+	case 'change-password':
+		$newPassword		=	$_POST['change-pw'];
+		$password			=	md5($newPassword);
+		$userID				= 	$_SESSION['id'];
+		
+		$query		= "update users(password)values('$password') where ID = '$userID'";
+		$res		= mysql_query($query);
+		
+		echo 'Your password has been updated.';
+		echo 'Click <a href="./">here</a> to go home.';	
+		
+	break;
+		
 	default:
 		
 		echo 'Restricted Page';
