d329ac3ac9608f0d0a840b29ca25c8d08066820e
centipedes <kylaslea@gmail.com>
2016-11-29 17:47:55 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,14 +4,14 @@
 
 	<head>
 		<link rel="stylesheet" type="text/css" href="./styles/stylesheet.css"/>
-		<title> COMP 353 - Ride Share Y'allllllllll </title>
+		<title> Velocity - COMP 353 </title>
 	</head>
 
 	<body>
 		<!-- BEGIN HEADER -->
 		<div id="header">
 			<div id="logo">
-				<center><h1>Ride Share Ap$$$$$$$</h1></center>
+				<center><h1>Velocity</h1></center>
 			</div>
 		</div>
 		<!-- END HEADER -->
@@ -28,10 +28,17 @@
 			<?php 
 				if(isset($_SESSION["userName"])) {
 					echo 'Welcome, '.$_SESSION["userName"]."<br />";
-					echo '<a href="./logout.php"> Logout </a><br />';
+					echo '<a href="./offerRide.php"> Offer Ride</a><br />';
+					echo '<a href="./rides.php"> View Ride Offers</a><br />';
+					echo '<a href="./myRides.php"> My Rides</a><br />';
+					echo '<a href="./messages.php"> Messages</a><br />';
+					echo '<a href="./profile.php"> Profile</a><br />';
+					echo '<a href="./balance.php"> Balance</a><br />';
+					echo '<a href="./logout.php"> Logout</a><br />';					
 				} else {
 					echo '<a href="./register.php"> Register </a><br />';
 					echo '<a href="./login.php"> Login </a><br />';
+					echo '<a href="./forgotPassword.php"> Forgot your Password? </a><br />';
 				}
 			?>
 			<!-- <a href="/logout.php"> Logout </a><br /> -->
